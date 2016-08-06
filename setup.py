a50bc1908661daf0014ab7068488abbe1245d368
Raphael Merx <raphael.merx@gmail.com>
2016-08-06 16:51:07 -0700

    Fix packaging to allow non-editable installation

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
-import os
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-os.chdir('OIPA')
-install_requirements = parse_requirements('requirements.txt', session=False)
+install_requirements = parse_requirements('OIPA/requirements.txt', session=False)
 requirements = [str(ir.req) for ir in install_requirements]
 
 setup(name='OIPA',
-      version='2.1',
-      description='',
+      version='2.1.3',
       author='Zimmerman & Zimmerman',
-      url="OIPA is an open-source framework that renders IATI compliant XML and \
+      description="OIPA is an open-source framework that renders IATI compliant XML and \
             related indicator #opendata into the OIPA datamodel for storage. \
             This ETL approach provides I/O using the OIPA Tastypie RESTless API (soon DRF!) \
             providing you with direct XML or JSON output. Does Django and MySQL. \
             Codebase maintained by Zimmerman & Zimmerman in Amsterdam. http://www.oipa.nl/",
-      packages=find_packages(),
+      url='https://github.com/catalpainternational/oipa',
+      packages=find_packages('OIPA'),  # iati, etc
+      package_dir={'': 'OIPA'},
       install_requires=requirements,
-     )
+      zip_safe=False
+      )
